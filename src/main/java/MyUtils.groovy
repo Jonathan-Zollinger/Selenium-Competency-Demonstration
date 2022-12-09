bb1c7d7a3c9178c0492c22a8f748009afb033b08
jonathan zollinger <jonathan.zollinger@outlook.com>
2022-12-09 14:46:10 -0700

    add MyUtils.groovy

---
+++ src/main/java/MyUtils.groovy
@@ -0,0 +1,32 @@
+import io.github.bonigarcia.wdm.WebDriverManager
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeOptions
+import org.openqa.selenium.support.ui.WebDriverWait
+
+import java.time.Duration;
+
+final class MyUtils {
+
+    static WebDriver getChromeDriver(){
+        // check latest version available here: https://chromedriver.storage.googleapis.com/index.html
+        WebDriverManager.chromedriver().browserVersion("99.0.4844.51").setup()
+        ChromeOptions options = new ChromeOptions()
+        options.addArguments("start-maximized")
+        options.addArguments("enable-automation")
+        options.addArguments("--no-sandbox")
+        options.addArguments("--disable-infobars")
+        options.addArguments("--disable-dev-shm-usage")
+        options.addArguments("--disable-browser-side-navigation")
+        options.addArguments("--disable-gpu")
+        return new ChromeDriver(options).get("https://www.webstaurantstore.com/")
+    }
+
+    static WebDriverWait getWebdriverWait(WebDriver driver){
+        new WebDriverWait(driver, Duration.ofSeconds(2));
+    }
+
+    static WebDriverWait getWebdriverWait(WebDriver driver, int seconds){
+        new WebDriverWait(driver, Duration.ofSeconds(seconds));
+    }
+}
