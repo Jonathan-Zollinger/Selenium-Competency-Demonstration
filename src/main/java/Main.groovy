2a5704682307429353138b1c7ae63c9f6f9144a4
jonathan zollinger <jonathan.zollinger@outlook.com>
2022-12-09 16:10:03 -0700

    search for steel tables

--- src/main/java/Main.groovy
+++ src/main/java/Main.groovy
@@ -11,19 +11,5 @@ class Main {
         driver.quit()
     }
 
-//    static WebDriver getChromeDriver() {
-//        // check latest version available here: https://chromedriver.storage.googleapis.com/index.html
-//        WebDriverManager.chromedriver().browserVersion("99.0.4844.51").setup()
-//        ChromeOptions options = new ChromeOptions()
-//        options.addArguments("start-maximized")
-//        options.addArguments("enable-automation")
-//        options.addArguments("--no-sandbox")
-//        options.addArguments("--disable-infobars")
-//        options.addArguments("--disable-dev-shm-usage")
-//        options.addArguments("--disable-browser-side-navigation")
-//        options.addArguments("--disable-gpu")
-//        return new ChromeDriver(options)
-//
-//    }
 }
 
