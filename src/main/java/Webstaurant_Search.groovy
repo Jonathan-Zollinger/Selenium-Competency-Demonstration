5bfe3150c2711e6c8e3704c8eb51ffa996ab7597
jonathan zollinger <jonathan.zollinger@outlook.com>
2022-12-09 12:16:50 -0700

    configure chromedriver
    - use WebDriver to configure chrome v99.0.4844.51
    - update WebstaurantSearch to use appropriate driver class

--- src/main/java/Webstaurant_Search.groovy
+++ src/main/java/Webstaurant_Search.groovy
@@ -1,4 +1,5 @@
-import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMap
+import org.openqa.selenium.WebDriver;
 
 class Webstaurant_Search {
     Map<String, String> selectors;
@@ -11,9 +12,10 @@ class Webstaurant_Search {
         this.selectors = ImmutableMap.of(
         //  ------ "my-selector-name", "xpath" ------
                 "search-bar-input", "input[id='searchval']",
-                "search-bar-submit-button", "button[value='Search']"
         )
     }
+
+    public searchForString(WebDriver driver)
 
 
 
