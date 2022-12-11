c30bfe2c9c1b4ba50f34edb216558e17410f0e3c
jonathan zollinger <jonathan.zollinger@outlook.com>
2022-12-10 17:50:48 -0700

    reformat log messages

--- src/main/java/Webstaurant.groovy
+++ src/main/java/Webstaurant.groovy
@@ -1,5 +1,6 @@
 import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
+import org.openqa.selenium.WebDriverException
 import org.openqa.selenium.support.ui.WebDriverWait
 
 import java.util.logging.Logger
@@ -8,6 +9,10 @@ class Webstaurant {
     WebDriver driver;
     WebDriverWait wait;
     Logger log = Logger.getLogger(this.class.getName())
+    private String logForElement = "the element found by the `%s` Selector"
+    private String openingLog = "Beginning attempt"
+    private String closingLog = "Finished attempt"
+    private String formattedLogStatement = "%s %s %s" // log relevance, log verb, log subject
     final protected By SEARCH_BAR_INPUT = By.xpath("//input[@id='searchval']")
     final protected By EXECUTE_SEARCH_BUTTON = By.xpath("//button[@value='Search']")
     final protected By PAGINATION_NUMBER_BUTTONS = By.xpath(
@@ -22,35 +27,82 @@ class Webstaurant {
     }
 
     protected searchForString(String findMe){
-        log.info(String.format("Beginning attempt to search for `%s`.", findMe))
+        logMethod(0, "searh for " + findMe)
         sendKeys(SEARCH_BAR_INPUT, findMe)
         click (EXECUTE_SEARCH_BUTTON)
-        log.info(String.format("Finished attempt to search for `%s`.", findMe))
+        logMethod(1, "searh for " + findMe)
         return this
     }
 
     protected sendKeys(By selector, String text) {
-        log.info(String.format("Beginning attempt to send keys `%s` to the `%s` element", text, selector))
+        action = String.format("to send key(s) %s to", text)
+        logMethod(0, action, selector)
         waitForElement (selector)
-        driver.findElement (selector).sendKeys(text)
-        log.info(String.format("Finished attempt to send keys `%s` to the `%s` element", text, selector))
+        try {
+            driver.findElement(selector).sendKeys(text)
+        }
+        catch(WebDriverException generalWebDriverException){
+            log.severe(String.format(logMethod(2,action, selector),generalWebDriverException.stackTrace)
+            )
+        }
+        logMethod(1, action, selector)
         return this
     }
 
     protected click(By selector) {
-        log.info(String.format("Beginning attempt to click the `%s` element", selector))
+        action = "click"
+        logMethod(0,"click", selector)
         waitForElement (selector)
-        driver.findElement (selector).click()
-        log.info(String.format("Finished attempt to click the `%s` element", selector))
+        try{
+            driver.findElement (selector).click()
+        }
+        catch(WebDriverException generalWebDriverException){
+            log.severe(String.format(logMethod(2,action, selector),generalWebDriverException.stackTrace)
+            )
+        }
+
+        logMethod(1,"click", selector)
         return this
     }
 
     protected waitForElement(By selector){
         assert null != wait
-        log.info(String.format("Beginning attempt to wait for the element `%s` to be clickable", selector))
-        wait.until (webDriver -> driver.findElement (selector))
-        log.info(String.format("Finish attempt to wait for the element `%s` to be clickable", selector))
+        String action = "wait to click"
+        logMethod(0, action , selector)
+        try{
+            wait.until (webDriver -> driver.findElement (selector))
+        }
+        catch(WebDriverException generalWebDriverException){
+            log.severe(String.format(logMethod(2,action, selector),generalWebDriverException.stackTrace)
+            )
+        }
+        logMethod(1, action , selector)
         return this
+    }
+
+    protected String logMethod(int startOrEndOrFail, String action, By selector){
+        String subject = String.format("the element found by the `%s` Selector", selector)
+        return logMethod(startOrEndOrFail, String.format("%s %s", action, subject))
+    }
+
+    protected String logMethod(int startOrEndOrFail, String action){
+        if (startOrEndOrFail.equals(2)){
+            return String.format("Failed to %s.%s", action, "\n\tStackTrace:\n\t%s")
+        }
+        return String.format("%s %s.", logMethod(startOrEndOrFail), action)
+    }
+
+    protected String logMethod(int startOrEndOrFail){
+        switch (start){
+            case 0:
+                return "Beginning attempt"
+                break
+            case 1:
+                return "Finished attempt"
+                break
+            default:
+                throw new IllegalStateException("logMethod(int) accepts only 0, 1 or 2.")
+        }
     }
 
 }
