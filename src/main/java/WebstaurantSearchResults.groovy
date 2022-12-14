3acfefe2c06e7cf3d69de04b21a9f996a5869d30
jonathan zollinger <jonathan.zollinger@outlook.com>
2022-12-14 12:50:53 -0700

    move page num methods to parent class

--- src/main/java/WebstaurantSearchResults.groovy
+++ src/main/java/WebstaurantSearchResults.groovy
@@ -3,77 +3,27 @@ import org.openqa.selenium.WebDriver
 import org.openqa.selenium.WebDriverException
 import org.openqa.selenium.WebElement
 
-class WebstaurantSearchResults extends Webstaurant{
+class WebstaurantSearchResults extends Webstaurant {
 
-    final private By ALL_RESULTS         = By.xpath("//div[@id='main']")
+    final private By ALL_RESULTS = By.xpath("//div[@id='main']")
     final private By RESULTS_DESCRIPTION = By.xpath("//div[@id='main']//a[@data-testid='itemDescription']")
 
-    WebstaurantSearchResults(WebDriver driver){
+    WebstaurantSearchResults(WebDriver driver) {
         super(driver)
     }
 
-    List<WebElement> GetPageResultsFromAllPages(){
+    List<WebElement> GetPageResultsFromAllPages() {
         List<WebElement> allResultsFromAllPages = getResults()
         if (isPaginationPresent()) {
             int lastPageNumber = getLastPageNumber() //last page number isn't available when on the last page
-            while (getThisPageNumber() != lastPageNumber) {
-                clickNextPageByNumber()
+            int thisPageNumber = getThisPageNumber()
+            while (thisPageNumber != lastPageNumber) {
+                clickNextPage()
                 allResultsFromAllPages.addAll(getResults())
+                thisPageNumber = getThisPageNumber()
             }
         }
         return allResultsFromAllPages
-    }
-
-    boolean isPaginationPresent() {
-        try{
-            driver.findElement {PAGINATION_NUMBERS}
-            log.info(String.format("Determined results span more than one page " +
-                    "because WebElements are found with the `%s` selector", PAGINATION_NUMBERS))
-            return true
-        }
-        catch(NoSuchElementException ignored) {
-            log.info(String.format("Determined results to span only one page " +
-                    "because no WebElements are found with the `%s` selector", PAGINATION_NUMBERS))
-            return false
-        }
-        catch(WebDriverException generalWebDriverException) {
-            log.severe(String.format("Encountered an exception other than 'NoSuchElementException'."))
-            throw generalWebDriverException
-        }
-    }
-    
-    int getThisPageNumber() {
-        assert isPaginationPresent()
-        try {
-            return Integer.parseInt(driver.findElement { PAGINATION_CURRENT_PAGE }.getText())
-        } 
-        catch (Exception pokemonCatch) { //catch 'em all
-            log.severe("Encountered an error when returning the current page number.")
-            throw pokemonCatch
-        }       
-    }
-    
-    int getLastPageNumber() {
-        assert isPaginationPresent()
-        try {
-            return Integer.parseInt(driver.findElement { PAGINATION_LAST_PAGE }.getText()
-            )
-        }
-        catch (Exception pokemonCatch) { 
-            log.severe("Encountered an Error when deriving the last page number.")
-            throw pokemonCatch
-        }
-    }
-    
-    void clickNextPageByNumber() {
-        try {
-            driver.findElement(By.xpath(String.format(XPATH_PAGINATION_FORMAT_NUMBER, (getThisPageNumber() + 1))))
-                    .click()
-        }
-        catch (Exception pokemonCatch) {
-            log.severe("Encountered an Error when clicking the next page by number.")
-            throw pokemonCatch
-        }
     }
 
     List<WebElement> getResults() {
@@ -85,6 +35,8 @@ class WebstaurantSearchResults extends Webstaurant{
             log.severe("Encountered an Error when returning a the webElements of all results' descriptions")
             throw pokemonCatch
         }
-        
+
     }
+
+
 }
